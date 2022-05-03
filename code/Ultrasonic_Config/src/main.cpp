c6b547f3ee616c2b7af5906ca161a470f270ac50
Jaggamasta <denisjaeger@web.de>
2022-05-03 02:29:41 +0200

    edited the Ultrasonic_Config project. Some ideas to solve this problem are written down in the commentaries

--- code/Ultrasonic_Config/src/main.cpp
+++ code/Ultrasonic_Config/src/main.cpp
@@ -1,15 +1,38 @@
 #include <Arduino.h>
+//#include <HCSR04.h>
 #include <HCSR04.h>
 
-HCSR04 hc(34, 35); //initialisation class HCSR04 (trig pin , echo pin)
+// Initialize sensor that uses digital pins 13 and 12.
+const byte triggerPin = 13;
+const byte echoPin = 12;
+float distance, ml;
+UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
-void setup()
-{
-    Serial.begin(9600);
+void setup () {
+    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
 }
 
-void loop()
-{
-    Serial.println(hc.dist()); // return curent distance in serial
-    delay(500);                 // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
+void loop () {
+    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+    distance = distanceSensor.measureDistanceCm();
+    ml = distance*0.02;
+    Serial.println(ml);
+    
+    //Serial.print(" in cm");
+    delay(1000);
+
+/*
+this case will be resolve with the arduino map() function
+all we need is to convert values from cm into ml by handling it
+
+3.1 cm = 100% of the tank volume, so the tank is full
+11.25 cm = 0% of the tank volume, so the tank is epmty
+
+maube try another function
+
+
+
+*/
+
+
 }
