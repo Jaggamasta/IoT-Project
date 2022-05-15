edb609429189295b593d989da8c404ca2492bc67
errorunbekannt <wiw19099@lehre.dhbw-stuttgart.de>
2022-05-15 13:18:26 +0200

    DHT_STEPPER_TELEGRAM überarbeitung und Fehlersuche

--- code/DHT_Stepper_Telegram/src/main.cpp
+++ code/DHT_Stepper_Telegram/src/main.cpp
@@ -15,6 +15,7 @@
 #define CHAT_ID "5322404568" 
 DHT dht(DHTPIN, DHTTYPE);
 
+
 const int SPU = 2048;
 #define IN1 32
 #define IN2 33
@@ -41,85 +42,13 @@ UniversalTelegramBot bot(BOTtoken, client);
 
 //Checks for new messages every 1 second.
 
-int botRequestDelay = 1000;
+int botRequestDelay = 100;
 
 unsigned long lastTimeBotRan;
-/*
-void handleNewMessages(int numNewMessages) {
 
-Serial.println("handleNewMessages");
-
-Serial.println(String(numNewMessages));
-
-for (int i=0; i<numNewMessages; i++) {
-
-String chat_id = String(bot.messages[i].chat_id);
-
-String text = bot.messages[i].text;
-
-String from_name = bot.messages[i].from_name;
-
-if (from_name == "") from_name = "Guest";
-if (text == "/temperature") {
-
-int t = dht.readTemperature();
-
-String temp = "Temperature : ";
-
-temp += int(t);
-
-temp +=" *C\n";
-
-bot.sendMessage(chat_id,temp, "");
-
-}
-
-
-
-if (text == "/humidity") {
-
-int h = dht.readHumidity();
-
-String temp = "Humidity: ";
-
-temp += int(h);
-
-temp += " %";
-
-bot.sendMessage(chat_id,temp, "");
-
-}
-
-if (text == "/start") {
-
-String welcome = "Welcome  " + from_name + ".\n";
-
-welcome += "/temperature : Temperature reading\n";
-
-welcome += "/humidity : Humiditiy reading\n";
-
-bot.sendMessage(chat_id, welcome, "Markdown");
-
-
-}
-
-
-
-}
-
-}
-*/
 void setup() {
-
-
 Motor.setSpeed(15);
 Serial.begin(9600);
-Serial.println();
-Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
-
-
-
-
 dht.begin();
 
 // This is the simplest way of getting this working
@@ -137,91 +66,81 @@ client.setInsecure();
 // connected
 
 WiFi.mode(WIFI_STA);
-
 WiFi.disconnect();
-
 delay(100);
 
 // attempt to connect to Wifi network:
-
 Serial.print("Connecting Wifi: ");
-
 Serial.println(ssid);
-
 WiFi.begin(ssid, password);
-
 while (WiFi.status() != WL_CONNECTED) {
-
 Serial.print(".");
-
 delay(500);
 
 }
 
 Serial.println("");
-
 Serial.println("WiFi connected");
-
 Serial.print("IP address: ");
-
 Serial.println(WiFi.localIP());
 
 }
 
 void loop() {
 
-
 //Motor.step(SPU);
-moving(-270);
+moving(-270); // von Referenzposition in Werkzeuglager
 delay(10000);
 //Motor.step(-SPU);
 moving(90); //Arbeitsposition
 delay(5000);
     
 int t = dht.readTemperature();
-
 int h = dht.readHumidity();
 
+// in der Arbeitsposition wird überprüft, ob das Werkzeug überhitzt
 float temperature = dht.readTemperature();
-if (temperature  > 25) {
+if (temperature  > 25) { 
+    int t = dht.readTemperature();
+    String temp = "Digga mir ist warm, mach mal Klima an: ";
+    temp += int(t);
+    temp +=" *C\n";
 
-int t = dht.readTemperature();
-String temp = "Digga mir ist warm, mach mal Klima an: ";
+    bot.sendMessage(CHAT_ID,temp, "");
+    delay(5000);
+    moving(90); //Kühlposition
+    delay(5000);
 
-temp += int(t);
+// in der Arbeitsposition wird erneut überprüft, ob das Werkzeug überhitzt
+    float temperature = dht.readTemperature();
+    if (temperature  > 25) { 
+        int t = dht.readTemperature();
+        String temp = "KOLLEGE IMMERNOCH, MACH MAL WAS ";
+        temp += int(t);
+        temp +=" *C\n";
+        bot.sendMessage(CHAT_ID,temp, "");
+        delay(5000);
 
-temp +=" *C\n";
+// in der Arbeitsposition wird erneut überprüft, ob das Werkzeug überhitzt
+        float temperature = dht.readTemperature();
+            if (temperature  > 25) {
 
-bot.sendMessage(CHAT_ID,temp, "");
-delay(5000);
-moving(90); //Arbeitsposition
-delay(5000);
-moving(-90); //Arbeitsposition
-delay(5000);
+                int t = dht.readTemperature();
+                String temp = "IDIOT ICH MACH JETZT EINFACH WEITER UND WENN DIE BUDE ABFACKELT...";
+                temp += int(t);
+                temp +=" *C\n";
+                bot.sendMessage(CHAT_ID,temp, "");
+                delay(5000);
+                moving(-90); //Arbeitsposition
+                delay(5000);
+
 
 }
+}
+}
+//Werkzeug bewegt sich nach es sich Abschluss der ARbeit wieder auf Ausgangsposition
 moving(180);
 delay(2000);
-/*
-if (millis() > lastTimeBotRan + botRequestDelay)  {
-
-int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-while(numNewMessages) {
-
-Serial.println("got response");
-
-handleNewMessages(numNewMessages);
-
-numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-
-}
-
-
-}
-*/
-
 
 lastTimeBotRan = millis();
 
